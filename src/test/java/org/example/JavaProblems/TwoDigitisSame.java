fb6336ec6fb5ffdc79c34da06bf41db5ccefd169
Srinath Keshetti <keshetti.srinath@gmail.com>
2026-05-03 10:43:33 +0530

    Fix TwoDigitsSame.java encoding and updates

--- src/test/java/org/example/JavaProblems/TwoDigitisSame.java
+++ src/test/java/org/example/JavaProblems/TwoDigitisSame.java
@@ -1,4 +1,22 @@
-package org.example.JavaProblems;
-
-public class TwoDigitisSame {
+public class TwoDigitisSame{
+    public static void main(String[] args) {
+        String str="12345";
+        while(str.length()>2){
+            StringBuilder sb=new StringBuilder();
+            for(int i=0;i<str.length()-1;i++){
+                int ch=str.charAt(i)-'0';
+                int ch1=str.charAt(i+1)-'0';
+                int sum=ch+ch1;
+                sb.append(sum%10);
+            }
+            str=sb.toString();
+            System.out.println(str);
+        }
+        if(str.charAt(0)==str.charAt(1)){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
 }
+
